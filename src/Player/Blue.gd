d957c7637f446d1ed173267634a03d8e3225dc48
Paul Mauviel <p.a.mauviel@gmail.com>
2020-09-18 20:48:37 -0400

    Update index.pck, Blue.gd, and PINK.gd

--- src/Player/Blue.gd
+++ src/Player/Blue.gd
@@ -2,7 +2,7 @@ extends "res://src/Player/PlayerShip.gd"
 
 func _ready():
 	#get_node("Sprite").fade_in("Idle", -1, -1, 2, '', GDDragonBones.FadeOut_None)
-	get_node("Sprite").fade_in("Idle", -1, -1, 15, '', GDDragonBones.FadeOut_None)
+	get_node("Sprite").fade_in("Idle", -1, -1, 15, '', GDArmatureDisplay.FadeOut_None)
 	get_node("Sprite").set_slot_by_item_name('Player', "blueface")	
 	current_gun = load("res://src/Pickups/BasicGun.tscn").instance()
 	add_child(current_gun)
--- src/Player/PINK.gd
+++ src/Player/PINK.gd
@@ -2,8 +2,8 @@ extends "res://src/Player/PlayerShip.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Sprite").fade_in("setPink", -1, 1, 1, '', GDDragonBones.FadeOut_All)
-	get_node("Sprite").fade_in("Idle", -1, -1, 2, '', GDDragonBones.FadeOut_SameLayer)
+	get_node("Sprite").fade_in("setPink", -1, 1, 1, '', GDArmatureDisplay.FadeOut_All)
+	get_node("Sprite").fade_in("Idle", -1, -1, 2, '', GDArmatureDisplay.FadeOut_SameLayer)
 	current_gun = load("res://src/Pickups/BasicGun.tscn").instance()
 	add_child(current_gun)
 	pass # Replace with function body.
